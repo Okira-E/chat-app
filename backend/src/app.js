1c898d84091704256e215a1ce0edea83b4b151cd
okira <omarrafat60@gmail.com>
2020-07-01 12:08:53 +0300

    Building...

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
 "use strict";
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 
-const PORT = process.env.PORT;
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`App is now listening on port ${PORT}`);
 });
