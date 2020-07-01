3f68ce012a6558459f6b5558322898b581b3fa53
okira <omarrafat60@gmail.com>
2020-07-01 12:35:01 +0300

    Buidling

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,23 @@
 "use strict";
 const express = require('express');
+const http = require('http');
 const cors = require('cors');
+const socketio = require('socket.io');
+
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 
 app.use(cors());
 
 
+io.on("connection", () => {
+    console.log("New connection");
+})
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`App is now listening on port ${PORT}`);
+server.listen(PORT, () => {
+    console.log(`App is now listening on port ${PORT}!`);
 });
