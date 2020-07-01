93eab38931f465ac594faad04ff5a2a1b51bcd1d
okira <omarrafat60@gmail.com>
2020-07-01 12:46:57 +0300

    Created the connection and setup the socket.io server

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { WebsocketService } from './services/websocket.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  constructor(private websocketService: WebsocketService) {}
+
+  ngOnInit(): void {
+    // Here we listen to the socket.io server
+    this.websocketService.listen("connection").subscribe(data => {
+      console.log(data);
+    });
+  }
 }
--- frontend/src/app/services/websocket.service.ts
+++ frontend/src/app/services/websocket.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
+import { Observable, Subscriber } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebsocketService {
+  private socket: any;
+  readonly uri: string = "http://localhost:3000";
 
-  constructor() { }
+  constructor() {
+    this.socket = io(this.uri);
+  }
+
+  listen(eventName: string) {
+    return new Observable((subscriber) => {
+      this.socket.on(eventName, (data) => {
+        subscriber.next(data);
+      });
+    });
+  }
+
+  emit(eventName: string, data: any) {
+    this.socket.emit(eventName, data);
+  }
+
 }
