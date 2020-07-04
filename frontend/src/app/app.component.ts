7d250c043e47ffa2cfd7cccf1b7e2618bd43ef5e
okira <omarrafat60@gmail.com>
2020-07-04 21:06:01 +0300

    Added the input and the send button and the functionality to emit messages to the server

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,23 +1,9 @@
-import { WebsocketService } from './services/websocket.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  constructor(private websocketService: WebsocketService) {}
-
-  ngOnInit(): void {
-    // Here we listen to the socket.io server
-    this.websocketService.listen("getData").subscribe(data => {
-      console.log(data);
-    });
-  }
-
-  increment(): void {
-    this.websocketService.emit("increment");
-  }
+export class AppComponent {
 }
---
+++ frontend/src/app/content/content.component.ts
@@ -0,0 +1,25 @@
+import { WebsocketService } from './../services/websocket.service';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+
+  constructor(private websocketService: WebsocketService) { }
+
+  ngOnInit(): void {
+    this.websocketService.listen('getData').subscribe((data) => {
+      console.log('ngOnInit ran');
+      console.log(data);
+    });
+  }
+
+  sendMessage(form: NgForm):void {
+    this.websocketService.emit("message", form.value.message);
+  }
+
+}
