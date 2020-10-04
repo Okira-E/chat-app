8e11139e19fea774be10362ec05a3bd3777ff3d6
Okira-E <omarrafat60@gmail.com>
2020-10-04 21:20:10 +0300

    Updated code

--- frontend/src/app/content/content.component.ts
+++ frontend/src/app/content/content.component.ts
@@ -2,37 +2,41 @@ import { WebsocketService } from './../services/websocket.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
-
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit, OnDestroy {
   public chat: string[];
+
   private onGetData: any;
   private onDisconnect: any;
 
   constructor(private websocketService: WebsocketService) {
     this.onGetData = this.websocketService.listen('getData');
-    this.onDisconnect = this.websocketService.listen("exit");
+    this.onDisconnect = this.websocketService.listen('exit');
   }
 
   ngOnInit(): void {
     this.onGetData.subscribe((data: []) => {
-      if (typeof data === "object") {
+      if (typeof data === 'object') {
         this.chat = data;
       } else {
         this.chat.push(data);
       }
     });
+
     this.onDisconnect.subscribe((message: string) => {
       console.log(message);
     });
   }
 
-  sendMessage(form: NgForm):void {
-    this.websocketService.emit("message", form.value.message);
+  sendMessage(form: NgForm): void {
+    if (!form.value.message) {
+      return;
+    }
+    this.websocketService.emit('message', form.value.message);
   }
 
   ngOnDestroy(): void {
